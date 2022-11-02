5daa62e3c3c8c990c812a2297a4156aca9fb9579
cjfinan <cjfinan98@gmail.com>
2022-11-02 10:19:32 +0000

    [ADD] PostAdmin and CommentAdmin to admin.py

--- reddit/admin.py
+++ reddit/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_date')
+    list_display = ('title', 'slug', 'status', 'created_date')
+    search_fields = ['title', 'content']
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_date', 'approved')
+    list_filter = ('approved', 'created_date')
+    search_fields = ['name', 'email_address', 'body']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)

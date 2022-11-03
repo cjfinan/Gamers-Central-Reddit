63f5283b7cdb3ce946014b05997e4b2dee2c4aca
cjfinan <cjfinan98@gmail.com>
2022-11-03 16:23:05 +0000

    [ADD] PostVotes to views ready to add functionality to votes

--- reddit/views.py
+++ reddit/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
@@ -34,3 +35,50 @@ class PostDetail(View):
                 "downvoted": downvoted,
                 "comment_form": CommentForm()
             },)
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_date')
+        upvoted = False
+        if post.upvotes.filter(id=self.request.user.id).exists():
+            upvoted = True
+        downvoted = False
+        if post.downvotes.filter(id=self.request.user.id).exists():
+            downvoted = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            'post_detail.html', {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "commented": False,
+                "upvoted": upvoted,
+                "downvoted": downvoted,
+                "comment_form": CommentForm()
+            },)
+
+
+class PostVotes(View):
+
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.upvotes.filter(id=request.user.id).exists():
+            post.upvotes.remove(request.user)
+        else:
+            post.upvotes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
